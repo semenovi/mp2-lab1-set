4d7c19727c663ee9548f33509d3db7c1b7899646
semenovi <t79601712352@gmail.com>
2018-09-19 18:43:48 +0300

    keyboard control

--- samples/life.cpp
+++ samples/life.cpp
@@ -6,7 +6,7 @@ void show(TSet& _field)
   for (int i = 0; i < H; i++)
   {
     for (int j = 0; j < W; j++)
-      cout << _field.IsMember(i * W + j);
+		cout << (_field.IsMember(i * W + j) ? "x" : "-");
     cout << "\n";
   }
 }
@@ -20,47 +20,48 @@ void kill(TSet& _field, int _x, int _y)
 }
 void next_generation(TSet& _field)
 {
-  int sum = 0;
-  for (int i = 1; i < H - 1; i++)
-  {
-    for (int j = 1; j < W - 1; j++)
-    {
-      for (int s = 0; s < 9; s++)
-      {
-        if (_field.IsMember(((i - 1) * W) + (j - 1)))
-          sum++;
-        if (_field.IsMember(((i - 1) * W) + (j)))
-          sum++;
-        if (_field.IsMember(((i - 1) * W) + (j + 1)))
-          sum++;
-        if (_field.IsMember((i * W) + (j - 1)))
-          sum++;
-        if (_field.IsMember((i * W) + (j + 1)))
-          sum++;
-        if (_field.IsMember(((i + 1) * W) + (j - 1)))
-          sum++;
-        if (_field.IsMember(((i + 1) * W) + (j)))
-          sum++;
-        if (_field.IsMember(((i + 1) * W) + (j + 1)))
-          sum++;
-      }
-      if (sum > 3)
-        kill(_field, i, j);
-      if (sum == 2)
-        spawn(_field, i, j);
-    }
-  }
+	int sum = 0;
+	for (int i = 1; i < H; i++)
+	{
+		for (int j = 1; j < W; j++)
+		{
+		
+		if (sum > 3)
+			kill(_field, i, j);
+		if (sum == 2)
+			spawn(_field, i, j);
+		}
+	}
+}
+int input_reaction(int _in, TSet& _field)
+{
+	int out = 1;
+	if (_in == 110)
+		next_generation(_field);
+	else if (_in == 115)
+		out = 0;
+	return out;
 }
 int main()
 {
-  int width = 10, height = 10;
-  TSet field(height * width);
-  spawn(field, 4, 4);
-  spawn(field, 4, 5);
-  spawn(field, 5, 4);
-  spawn(field, 5, 5);
-  show(field);
-  next_generation(field);
-  show(field);
-  return 0;
+	int state = 0, in = 0;
+	TSet field(H * W);
+	spawn(field, 4, 4);
+	spawn(field, 4, 5);
+	spawn(field, 5, 4);
+	spawn(field, 5, 5);
+	show(field);
+	while (1)
+	{
+		in = getchar();
+		state = input_reaction(in, field);
+		if (state)
+		{
+			system("cls");
+			show(field);
+		}
+		else
+			break;
+	}
+	return 0;
 }
